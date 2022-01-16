e3c74037de7405ea6c9d49c51dbf9f093d99c22b
wimilee <wimilee@163.com>
2022-01-16 19:45:05 +0800

    搭建了api平台调用banner，但是报700无数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,93 +1,46 @@
-// pages/index/index.js
 const WXAPI = require('apifm-wxapi')
 const CONFIG = require('../../config.js')
+WXAPI.init('wimilee')
+//获取应用实例
 var app = getApp();
-
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     banners:[],
-    swiperMaxNumber:0,
-    swiperCurrent:0
+    swiperMaxNumber: 0,
+    swiperCurrent: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      const _this = this
+  onLoad:function(){
+    const _this = this
+      // 展示启动页
+      WXAPI.banners({
+        type: 'app' 
+      }).then(function (res) {
+        console.log(res)
+          _this.setData({
+            banners: res.data,
+            swiperMaxNumber: res.data.length
+          });
+        }
+      )
+    },
+  onShow:function(){
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  goMainPage: function (e) {
-    const http = new XMLHttpRequest()
-    const url = 'https://www.baidu.com'
-    http.open('GET',url)
-    http.send()
-  },
-  imageIndicator: function (e) {
-    if(this.data.swiperCurrent+1!=this.data.swiperMaxNumber){
-      wx.showToast({
-        title: '右滑进入',
-        icon:'none'
-      })
-    }
-  },
-  swiperChange: function (e) {
+  swiperchange: function (e) {
     console.log(e.detail.current)
     this.setData({
       swiperCurrent: e.detail.current
     })
+  },
+  goMainPage: function (e) {
+    
+  },
+  imageIndicator(){
+    if (this.data.swiperCurrent + 1 != this.data.swiperMaxNumber) {
+      wx.showToast({
+        title: '左滑进入',
+        icon: 'none',
+      })
+    }
   }
-  
-})
+});
